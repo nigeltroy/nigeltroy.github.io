821521fe3cc961da083b2d1896fe4983c6ce9cd2
nigeltroy <nigel.troy1@gmail.com>
2020-07-18 14:07:20 -0400

    Updates

---
+++ precache-manifest.405ac8a782d806faeb098c8829064a88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97fd2b81c0f072aba0dc5b29dc50e428",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1635dbbc39c0f566617",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b28d4f00b1d6728a3417",
+    "url": "/static/js/2.6a6b0bab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6a6b0bab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1635dbbc39c0f566617",
+    "url": "/static/js/main.496b9264.chunk.js"
+  },
+  {
+    "revision": "2f2f33b5ffb95e9a460d",
+    "url": "/static/js/runtime-main.9f759918.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
